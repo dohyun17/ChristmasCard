cf47d13491829a9273ed2332c82ab58790c9d990
dohyun17 <dh2842@kakao.com>
2024-12-14 10:38:18 +0900

    크리스마스 편지 기능추가

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from app import app, db
 from app.models import Letter
 
@@ -14,6 +14,7 @@ def write_letter():
         letter = Letter(sender=sender, content=content)
         db.session.add(letter)
         db.session.commit()
+        flash('편지가 성공적으로 작성되었습니다.')
         return redirect(url_for('letter_list'))
     return render_template('write_letter.html')
 
@@ -26,3 +27,22 @@ def letter_list():
 def letter_detail(id):
     letter = Letter.query.get_or_404(id)
     return render_template('letter_detail.html', letter=letter)
+
+@app.route('/letter/<int:id>/edit', methods=['GET', 'POST'])
+def edit_letter(id):
+    letter = Letter.query.get_or_404(id)
+    if request.method == 'POST':
+        letter.sender = request.form['sender']
+        letter.content = request.form['content']
+        db.session.commit()
+        flash('편지가 성공적으로 수정되었습니다.')
+        return redirect(url_for('letter_detail', id=letter.id))
+    return render_template('edit_letter.html', letter=letter)
+
+@app.route('/letter/<int:id>/delete', methods=['POST'])
+def delete_letter(id):
+    letter = Letter.query.get_or_404(id)
+    db.session.delete(letter)
+    db.session.commit()
+    flash('편지가 성공적으로 삭제되었습니다.')
+    return redirect(url_for('letter_list'))
